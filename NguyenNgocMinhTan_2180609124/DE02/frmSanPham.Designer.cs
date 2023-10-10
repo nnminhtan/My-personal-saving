c480a3291abbcabf2c52ac6cb0fe01276a9af1e1
Tan Nguyen <93578088+nnminhtan@users.noreply.github.com>
2023-10-10 18:32:48 +0700

    Adding GUI layer

---
+++ NguyenNgocMinhTan_2180609124/DE02/frmSanPham.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace DE02
+{
+    partial class frmSanPham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.btFind = new System.Windows.Forms.Button();
+            this.lvSanPham = new System.Windows.Forms.ListView();
+            this.MaSP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TenSP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NgayNhap = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MaLoai = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btThem = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btLuu = new System.Windows.Forms.Button();
+            this.btKLuu = new System.Windows.Forms.Button();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.lblMaSP = new System.Windows.Forms.Label();
+            this.txtMaSP = new System.Windows.Forms.TextBox();
+            this.lblTenSP = new System.Windows.Forms.Label();
+            this.lblNgayNhap = new System.Windows.Forms.Label();
+            this.txtTenSP = new System.Windows.Forms.TextBox();
+            this.lblLoaiSP = new System.Windows.Forms.Label();
+            this.dtNgayNhap = new System.Windows.Forms.DateTimePicker();
+            this.cboLoaiSP = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(106, 75);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(321, 31);
+            this.txtFind.TabIndex = 0;
+            // 
+            // btFind
+            // 
+            this.btFind.Location = new System.Drawing.Point(456, 75);
+            this.btFind.Name = "btFind";
+            this.btFind.Size = new System.Drawing.Size(106, 34);
+            this.btFind.TabIndex = 1;
+            this.btFind.Text = "Find";
+            this.btFind.UseVisualStyleBackColor = true;
+            this.btFind.Click += new System.EventHandler(this.btFind_Click);
+            // 
+            // lvSanPham
+            // 
+            this.lvSanPham.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.MaSP,
+            this.TenSP,
+            this.NgayNhap,
+            this.MaLoai});
+            this.lvSanPham.HideSelection = false;
+            this.lvSanPham.Location = new System.Drawing.Point(58, 136);
+            this.lvSanPham.Name = "lvSanPham";
+            this.lvSanPham.Size = new System.Drawing.Size(1081, 661);
+            this.lvSanPham.TabIndex = 2;
+            this.lvSanPham.UseCompatibleStateImageBehavior = false;
+            this.lvSanPham.View = System.Windows.Forms.View.Details;
+            this.lvSanPham.SelectedIndexChanged += new System.EventHandler(this.lvSanPham_SelectedIndexChanged);
+            // 
+            // MaSP
+            // 
+            this.MaSP.Text = "MaSP";
+            // 
+            // TenSP
+            // 
+            this.TenSP.Text = "TenSP";
+            this.TenSP.Width = 100;
+            // 
+            // NgayNhap
+            // 
+            this.NgayNhap.Text = "NgayNhap";
+            this.NgayNhap.Width = 150;
+            // 
+            // MaLoai
+            // 
+            this.MaLoai.Text = "MaLoai";
+            // 
+            // btThem
+            // 
+            this.btThem.Location = new System.Drawing.Point(58, 815);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(157, 55);
+            this.btThem.TabIndex = 3;
+            this.btThem.Text = "Them";
+            this.btThem.UseVisualStyleBackColor = true;
+            this.btThem.Click += new System.EventHandler(this.btThem_Click);
+            // 
+            // btXoa
+            // 
+            this.btXoa.Location = new System.Drawing.Point(426, 815);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(157, 55);
+            this.btXoa.TabIndex = 3;
+            this.btXoa.Text = "Xoa";
+            this.btXoa.UseVisualStyleBackColor = true;
+            this.btXoa.Click += new System.EventHandler(this.btXoa_Click);
+            // 
+            // btSua
+            // 
+            this.btSua.Location = new System.Drawing.Point(242, 815);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(157, 55);
+            this.btSua.TabIndex = 3;
+            this.btSua.Text = "Sua";
+            this.btSua.UseVisualStyleBackColor = true;
+            this.btSua.Click += new System.EventHandler(this.btSua_Click);
+            // 
+            // btLuu
+            // 
+            this.btLuu.Enabled = false;
+            this.btLuu.Location = new System.Drawing.Point(610, 815);
+            this.btLuu.Name = "btLuu";
+            this.btLuu.Size = new System.Drawing.Size(157, 55);
+            this.btLuu.TabIndex = 3;
+            this.btLuu.Text = "Luu";
+            this.btLuu.UseVisualStyleBackColor = true;
+            this.btLuu.Click += new System.EventHandler(this.btLuu_Click);
+            // 
+            // btKLuu
+            // 
+            this.btKLuu.Enabled = false;
+            this.btKLuu.Location = new System.Drawing.Point(794, 815);
+            this.btKLuu.Name = "btKLuu";
+            this.btKLuu.Size = new System.Drawing.Size(157, 55);
+            this.btKLuu.TabIndex = 3;
+            this.btKLuu.Text = "K.Luu";
+            this.btKLuu.UseVisualStyleBackColor = true;
+            this.btKLuu.Click += new System.EventHandler(this.btKLuu_Click);
+            // 
+            // btThoat
+            // 
+            this.btThoat.Location = new System.Drawing.Point(978, 815);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(157, 55);
+            this.btThoat.TabIndex = 3;
+            this.btThoat.Text = "Thoat";
+            this.btThoat.UseVisualStyleBackColor = true;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // lblMaSP
+            // 
+            this.lblMaSP.AutoSize = true;
+            this.lblMaSP.Location = new System.Drawing.Point(1184, 187);
+            this.lblMaSP.Name = "lblMaSP";
+            this.lblMaSP.Size = new System.Drawing.Size(70, 25);
+            this.lblMaSP.TabIndex = 4;
+            this.lblMaSP.Text = "MaSP";
+            // 
+            // txtMaSP
+            // 
+            this.txtMaSP.Location = new System.Drawing.Point(1327, 186);
+            this.txtMaSP.Name = "txtMaSP";
+            this.txtMaSP.Size = new System.Drawing.Size(369, 31);
+            this.txtMaSP.TabIndex = 5;
+            // 
+            // lblTenSP
+            // 
+            this.lblTenSP.AutoSize = true;
+            this.lblTenSP.Location = new System.Drawing.Point(1184, 268);
+            this.lblTenSP.Name = "lblTenSP";
+            this.lblTenSP.Size = new System.Drawing.Size(77, 25);
+            this.lblTenSP.TabIndex = 4;
+            this.lblTenSP.Text = "TenSP";
+            // 
+            // lblNgayNhap
+            // 
+            this.lblNgayNhap.AutoSize = true;
+            this.lblNgayNhap.Location = new System.Drawing.Point(1184, 349);
+            this.lblNgayNhap.Name = "lblNgayNhap";
+            this.lblNgayNhap.Size = new System.Drawing.Size(113, 25);
+            this.lblNgayNhap.TabIndex = 4;
+            this.lblNgayNhap.Text = "NgayNhap";
+            // 
+            // txtTenSP
+            // 
+            this.txtTenSP.Location = new System.Drawing.Point(1327, 268);
+            this.txtTenSP.Name = "txtTenSP";
+            this.txtTenSP.Size = new System.Drawing.Size(369, 31);
+            this.txtTenSP.TabIndex = 5;
+            // 
+            // lblLoaiSP
+            // 
+            this.lblLoaiSP.AutoSize = true;
+            this.lblLoaiSP.Location = new System.Drawing.Point(1184, 430);
+            this.lblLoaiSP.Name = "lblLoaiSP";
+            this.lblLoaiSP.Size = new System.Drawing.Size(81, 25);
+            this.lblLoaiSP.TabIndex = 4;
+            this.lblLoaiSP.Text = "LoaiSP";
+            // 
+            // dtNgayNhap
+            // 
+            this.dtNgayNhap.Location = new System.Drawing.Point(1327, 350);
+            this.dtNgayNhap.Name = "dtNgayNhap";
+            this.dtNgayNhap.Size = new System.Drawing.Size(369, 31);
+            this.dtNgayNhap.TabIndex = 6;
+            // 
+            // cboLoaiSP
+            // 
+            this.cboLoaiSP.FormattingEnabled = true;
+            this.cboLoaiSP.Location = new System.Drawing.Point(1327, 432);
+            this.cboLoaiSP.Name = "cboLoaiSP";
+            this.cboLoaiSP.Size = new System.Drawing.Size(369, 33);
+            this.cboLoaiSP.TabIndex = 7;
+            // 
+            // frmSanPham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1838, 948);
+            this.Controls.Add(this.cboLoaiSP);
+            this.Controls.Add(this.dtNgayNhap);
+            this.Controls.Add(this.txtTenSP);
+            this.Controls.Add(this.txtMaSP);
+            this.Controls.Add(this.lblLoaiSP);
+            this.Controls.Add(this.lblNgayNhap);
+            this.Controls.Add(this.lblTenSP);
+            this.Controls.Add(this.lblMaSP);
+            this.Controls.Add(this.btSua);
+            this.Controls.Add(this.btThoat);
+            this.Controls.Add(this.btKLuu);
+            this.Controls.Add(this.btLuu);
+            this.Controls.Add(this.btXoa);
+            this.Controls.Add(this.btThem);
+            this.Controls.Add(this.lvSanPham);
+            this.Controls.Add(this.btFind);
+            this.Controls.Add(this.txtFind);
+            this.Name = "frmSanPham";
+            this.Text = "Danh Muc San Pham";
+            this.Load += new System.EventHandler(this.frmSanPham_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Button btFind;
+        private System.Windows.Forms.ListView lvSanPham;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.Button btLuu;
+        private System.Windows.Forms.Button btKLuu;
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Label lblMaSP;
+        private System.Windows.Forms.TextBox txtMaSP;
+        private System.Windows.Forms.Label lblTenSP;
+        private System.Windows.Forms.Label lblNgayNhap;
+        private System.Windows.Forms.TextBox txtTenSP;
+        private System.Windows.Forms.Label lblLoaiSP;
+        private System.Windows.Forms.DateTimePicker dtNgayNhap;
+        private System.Windows.Forms.ComboBox cboLoaiSP;
+        private System.Windows.Forms.ColumnHeader MaSP;
+        private System.Windows.Forms.ColumnHeader TenSP;
+        private System.Windows.Forms.ColumnHeader NgayNhap;
+        private System.Windows.Forms.ColumnHeader MaLoai;
+    }
+}
+
---
+++ NguyenNgocMinhTan_2180609124/DE02/frmSanPham.cs
@@ -0,0 +1,316 @@
+﻿using DE02.DAL.Entities;
+using DE02.BUS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.ListViewItem;
+using System.Runtime.Remoting.Contexts;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.Entity;
+using Database = DE02.DAL.Entities.Database;
+
+namespace DE02
+{
+    public partial class frmSanPham : Form
+    {
+        Database db = new Database();
+        private readonly SanPhamService sanPhamService = new SanPhamService(); 
+        private readonly LoaiSPService loaiSPService = new LoaiSPService();
+        public int CountRow;
+
+        public int OriginalRow;
+        public frmSanPham()
+        {
+            InitializeComponent();
+        }
+        private void frmSanPham_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var listSanPham = sanPhamService.GetAll();
+                var listLoaiSP = loaiSPService.GetAll();
+                BindList(listSanPham);
+                fillCboLoaiSP(listLoaiSP);
+                OriginalRow = lvSanPham.Items.Count;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void fillCboLoaiSP(List<LoaiSP> listLoaiSP)
+        {
+            this.cboLoaiSP.DataSource = listLoaiSP;
+            this.cboLoaiSP.DisplayMember = "TenLoai";
+            this.cboLoaiSP.ValueMember = "MaLoai";
+        }
+        private void BindList(List<SanPham> listSanPham)
+        {
+            // Bind the ListView control to the list of students.
+            lvSanPham.Items.Clear();
+            foreach (SanPham sp in listSanPham)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = sp.MaSP;
+                item.SubItems.Add(new ListViewSubItem(item, sp.TenSP));
+                item.SubItems.Add(new ListViewSubItem(item, sp.NgayNhap.ToString()));
+                item.SubItems.Add(new ListViewSubItem(item, sp.LoaiSP?.TenLoai));
+                lvSanPham.Items.Add(item);
+            }
+        }
+
+        private void lvSanPham_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvSanPham.SelectedItems.Count > 0)
+            {
+                // Get the selected item in the ListView.
+                ListViewItem item = lvSanPham.SelectedItems[0];
+
+                // Get the data from the selected item.
+                string maSV = item.Text;
+                string hoTenSV = item.SubItems[1].Text;
+                DateTime ngaySinh;
+                string tenLop = item.SubItems[3].Text;
+
+                // Set the values of the TextBox, DateTimePicker and ComboBox.
+                txtMaSP.Text = maSV;
+                txtTenSP.Text = hoTenSV;
+                if (DateTime.TryParse(item.SubItems[2].Text.ToString(), out ngaySinh))
+                {
+                    dtNgayNhap.Value = ngaySinh;
+                }
+                cboLoaiSP.Text = tenLop;
+            }
+        }
+
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = new ListViewItem(txtMaSP.Text);
+            item.SubItems.Add(txtTenSP.Text);
+            item.SubItems.Add(dtNgayNhap.Value.ToString());
+            item.SubItems.Add(cboLoaiSP.Text);
+            lvSanPham.Items.Add(item);
+            txtMaSP.Text = null;
+            txtTenSP.Text = null;
+            dtNgayNhap.Value = DateTime.Now;
+            cboLoaiSP.SelectedIndex = 0;
+            CountRow = lvSanPham.Items.Count;
+            // Update the save button state.
+            UpdateSaveandUnsaveButtonState();
+        }
+
+        //private void RefreshData()
+        //{
+        //    var listStudents = sanPhamService.GetAll();
+        //    BindList(listStudents);
+        //    lvSanPham.Update();
+        //    lvSanPham.Refresh();
+        //}
+
+        private void UpdateSaveandUnsaveButtonState()
+        {
+            // Enable the save button only if there are any unsaved changes in the ListView.
+            btLuu.Enabled = lvSanPham.Items.Count >= OriginalRow || lvSanPham.Items.Count < OriginalRow;
+            btKLuu.Enabled = lvSanPham.Items.Count >= OriginalRow || lvSanPham.Items.Count < OriginalRow;
+        }
+
+        private void btSua_Click(object sender, EventArgs e)
+        {
+            if (lvSanPham.SelectedItems.Count > 0)
+            {
+                ListViewItem lv = lvSanPham.SelectedItems[0];
+                lv.SubItems[1].Text = txtTenSP.Text;
+                lv.SubItems[2].Text = dtNgayNhap.Value.ToString();
+                lv.SubItems[3].Text = cboLoaiSP.Text;
+                txtMaSP.Text = null;
+                txtTenSP.Text = null;
+                dtNgayNhap.Value = DateTime.Now;
+                cboLoaiSP.SelectedIndex = 0;
+                UpdateSaveandUnsaveButtonState();
+            }
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show(
+                    "Do you want to delete this?",
+                    "Title",
+                    MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (lvSanPham.SelectedItems.Count > 0)
+                {
+                    // Get the selected item(s)
+                    ListViewItem selectedItem = lvSanPham.SelectedItems[0];
+
+                    // Remove the selected item from the ListView
+                    lvSanPham.Items.Remove(selectedItem);
+                }
+                UpdateSaveandUnsaveButtonState();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btLuu_Click(object sender, EventArgs e)
+        {
+            // Get the new SanPham objects from the ListView control.
+            List<SanPham> newSP = new List<SanPham>();
+
+            foreach (ListViewItem item in lvSanPham.Items)
+            {
+                SanPham SP = new SanPham();
+                SP.MaSP = item.Text;
+                SP.TenSP = item.SubItems[1].Text;
+
+                DateTime ngayNhap;
+                if (DateTime.TryParse(item.SubItems[2].Text, out ngayNhap))
+                {
+                    SP.NgayNhap = ngayNhap;
+                }
+                // Rest of your code for populating SP.MaLoai goes here...
+                var tenLoai = item.SubItems[3].Text;
+
+                // Query the related LoaiSP table to get the MaLoai based on TenLoai
+                var maLoai = db.LoaiSP
+                    .Where(loai => loai.TenLoai == tenLoai)
+                    .Select(loai => loai.MaLoai)
+                    .FirstOrDefault();
+
+                if (!string.IsNullOrEmpty(maLoai))
+                {
+                    SP.MaLoai = maLoai;
+                }
+
+                newSP.Add(SP);
+            }
+
+            // Get the MaSP values from the newSP list
+            var maSPsInNewSP = newSP.Select(sp => sp.MaSP).ToList();
+
+            // Retrieve existing entities from the database that have MaSP in the newSP list
+            var existingEntities = db.SanPham.Where(s => maSPsInNewSP.Contains(s.MaSP)).ToList();
+
+            // Update existing entities based on the newSP list
+            foreach (var existingEntity in existingEntities)
+            {
+                var correspondingNewSP = newSP.FirstOrDefault(sp => sp.MaSP == existingEntity.MaSP);
+                if (correspondingNewSP != null)
+                {
+                    // Update existingEntity properties with correspondingNewSP values
+                    existingEntity.TenSP = correspondingNewSP.TenSP;
+                    existingEntity.NgayNhap = correspondingNewSP.NgayNhap;
+                   // existingEntity.MaLoai = correspondingNewSP.MaLoai;
+                    // Update other properties as needed...
+                }
+            }
+
+            // Remove entities that are in the database but not in the newSP list
+            var entitiesToDelete = db.SanPham.Where(s => !maSPsInNewSP.Contains(s.MaSP)).ToList();
+            db.SanPham.RemoveRange(entitiesToDelete);
+
+            // Add new entities that are in the newSP list but not in the database
+            var entitiesToAdd = newSP.Where(sp => !existingEntities.Any(ee => ee.MaSP == sp.MaSP)).ToList();
+            db.SanPham.AddRange(entitiesToAdd);
+
+            // Save changes to the database
+            db.SaveChanges();
+        }
+
+        private void btKLuu_Click(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            var listSanPham = sanPhamService.GetAll();
+            var listLoaiSP = loaiSPService.GetAll();
+            BindList(listSanPham);
+        }
+
+        private void btThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show(
+                    "Do you want to quit?",
+                    "Title",
+                    MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btFind_Click(object sender, EventArgs e)
+        {
+            List<SanPham> searchStudents = new List<SanPham>();
+            string name = txtFind.Text;
+            foreach (ListViewItem item in lvSanPham.Items)
+            {
+                var ten = item.SubItems[1].Text;
+                if (ten != null && ten.ToLower().Contains(name.ToLower()))
+                {
+                    SanPham student = new SanPham();
+                    student.MaSP = item.Text;
+                    student.TenSP = item.SubItems[1].Text;
+                    DateTime ngaySinh;
+                    if (DateTime.TryParse(item.SubItems[2].Text.ToString(), out ngaySinh))
+                    {
+                        dtNgayNhap.Value = ngaySinh;
+                    }
+                    student.NgayNhap = dtNgayNhap.Value;
+                    string maSV = item.Text.ToString();
+                    var maLop = sanPhamService.FindByMaSP(maSV)?.MaLoai?.ToString();
+                    if (maLop != null)
+                    {
+                        student.MaLoai = item.SubItems[3].Text;
+                    }
+                    searchStudents.Add(student);
+                }
+
+            }
+            if (searchStudents.Count == 0)
+            {
+                MessageBox.Show("khong tim thay sinh vien");
+            }
+            else
+            {
+                // Convert the list of students to an array of ListViewItem objects.
+                ListViewItem[] listViewItems = ConvertStudentsToListViewItems(searchStudents);
+                lvSanPham.Items.Clear();
+                lvSanPham.Items.AddRange(listViewItems);
+                txtFind = null;
+            }
+        }
+        private ListViewItem[] ConvertStudentsToListViewItems(List<SanPham> students)
+        {
+            ListViewItem[] listViewItems = new ListViewItem[students.Count];
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                ListViewItem listViewItem = new ListViewItem();
+                listViewItem.Text = students[i].MaSP;
+                listViewItem.SubItems.Add(students[i].TenSP);
+                listViewItem.SubItems.Add(students[i].NgayNhap.ToString());
+                listViewItem.SubItems.Add(students[i].MaLoai);
+
+                listViewItems[i] = listViewItem;
+            }
+
+            return listViewItems;
+        }
+    }
+}
